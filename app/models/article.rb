d7da4755c4b83d16b75e1a923d7b2a10e503cd4a
Jennifer Han <jhan2@andrew.cmu.edu>
2015-04-21 20:25:34 -0400

    added controller and view for Articles

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ActiveRecord::Base
+
+  # Relationships 
+  belongs_to :category
+
+  # Validations
+  validates_presence_of :title, :content
+
+  # Scopes 
+  scope :alphabetical, order('title')
+  scope :active, where('active = ?', true)
+
+  
+end
